a6a66cf1457a026ca25d98c5bd4c4e4245bfcb09
Dan Grahn <dan.grahn@altamiracorp.com>
2022-11-30 15:26:01 -0500

    Fix returns

--- run_test.py
+++ run_test.py
@@ -36,32 +36,24 @@ def model4(task):
     pipe = codebert_dataset(task, trans, BATCH_SIZE)
     return models.model4_codebert(), pipe
 
-model1_task1 = lambda: models.model1_mlp((3, 2)), task1_basic()
-model1_task2 = lambda: models.model1_mlp((9, 2)), task2_basic()
-model1_task3 = lambda: models.model1_mlp((34, 2)), task3_basic()
-model1_task4 = lambda: models.model1_mlp((26, 2)), task4_basic()
-model2_task1 = lambda: models.model2_cnn((3, 2)), task1_basic()
-model2_task2 = lambda: models.model2_cnn((9, 2)), task2_basic()
-model2_task3 = lambda: models.model2_cnn((34, 2)), task3_basic()
-model2_task4 = lambda: models.model2_cnn((26, 2)), task4_basic()
-model3_task1 = lambda: models.model3_lstm((3, 2)), task1_basic()
-model3_task2 = lambda: models.model3_lstm((9, 2)), task2_basic()
-model3_task3 = lambda: models.model3_lstm((34, 2)), task3_basic()
-model3_task4 = lambda: models.model3_lstm((26, 2)), task4_basic()
+model1_task1 = lambda: (models.model1_mlp((3, 2)), task1_basic())
+model1_task2 = lambda: (models.model1_mlp((9, 2)), task2_basic())
+model1_task3 = lambda: (models.model1_mlp((34, 2)), task3_basic())
+model1_task4 = lambda: (models.model1_mlp((26, 2)), task4_basic())
+model2_task1 = lambda: (models.model2_cnn((3, 2)), task1_basic())
+model2_task2 = lambda: (models.model2_cnn((9, 2)), task2_basic())
+model2_task3 = lambda: (models.model2_cnn((34, 2)), task3_basic())
+model2_task4 = lambda: (models.model2_cnn((26, 2)), task4_basic())
+model3_task1 = lambda: (models.model3_lstm((3, 2)), task1_basic())
+model3_task2 = lambda: (models.model3_lstm((9, 2)), task2_basic())
+model3_task3 = lambda: (models.model3_lstm((34, 2)), task3_basic())
+model3_task4 = lambda: (models.model3_lstm((26, 2)), task4_basic())
 model4_task1 = lambda: model4(generator.Task1Generator())
 model4_task2 = lambda: model4(generator.Task2Generator())
 model4_task3 = lambda: model4(generator.Task3Generator())
 model4_task4 = lambda: model4(generator.Task4Generator())
 
-def model5_task1():
-    task = generator.Task1Generator()
-    trans = translators.NetworkXTranslator()
-    pipe = dataset_generator(task, trans)
-
-
-    return model, pipe
-
-
+def model5_task1(): raise NotImplementedError()
 def model5_task2(): raise NotImplementedError()
 def model5_task3(): raise NotImplementedError()
 def model5_task4(): raise NotImplementedError()
