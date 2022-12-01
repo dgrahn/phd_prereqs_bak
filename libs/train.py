c509c0f5902e92306a3567d984ad5790f84da650
Dan Grahn <dan.grahn@altamiracorp.com>
2022-12-01 11:40:04 -0500

    Train updates

--- libs/train.py
+++ libs/train.py
@@ -1,6 +1,8 @@
+import spektral
 import tensorflow as tf
 
-def do_train(model, pipe, batch_size):
+def do_train(model, pipe, batch_size, epochs=10, steps_per_epoch=10_000,
+    validation_steps=1_000):
     # Compile the model
     model.compile(optimizer='adam',
                 loss='binary_crossentropy',
@@ -12,12 +14,18 @@ def do_train(model, pipe, batch_size):
                     tf.keras.metrics.FalseNegatives(name='fn'),
                 ])
 
+    if isinstance(pipe, spektral.data.loaders.Loader):
+        print('-' * 30)
+        print('Spektral loader identified, changing batch size to 1.')
+        print('-' * 30)
+        batch_size = 1
+
     # Train the model
     model.fit(
         pipe,
         batch_size=batch_size,
-        epochs = 10,
-        steps_per_epoch = 10_000,
+        epochs = epochs,
+        steps_per_epoch = steps_per_epoch,
         validation_data = pipe,
-        validation_steps = 1_000,
+        validation_steps = validation_steps,
     )
