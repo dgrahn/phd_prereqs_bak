9fcba44b4834ecd1d7ace9969ec258b15658c4bc
Dan Grahn <dan.grahn@altamiracorp.com>
2022-12-02 10:02:58 -0500

    Add task 5

--- run_test.py
+++ run_test.py
@@ -20,6 +20,7 @@ task1_basic = lambda: basic(generator.Task1Generator())
 task2_basic = lambda: basic(generator.Task2Generator())
 task3_basic = lambda: basic(generator.Task3Generator(), max_size=34)
 task4_basic = lambda: basic(generator.Task4Generator())
+task5_basic = lambda: basic(generator.Task5Generator())
 
 def model4(task):
     trans = translators.PythonTranslator()
@@ -35,26 +36,37 @@ def model5(task):
     model = models.model5_gnn(dataset.n_node_features, dataset.n_edge_features)
     return model, loader
 
-model1_task1 = lambda: (models.model1_mlp((3, 2)), task1_basic())
-model1_task2 = lambda: (models.model1_mlp((9, 2)), task2_basic())
-model1_task3 = lambda: (models.model1_mlp((34, 2)), task3_basic())
-model1_task4 = lambda: (models.model1_mlp((26, 2)), task4_basic())
-model2_task1 = lambda: (models.model2_cnn((3, 2)), task1_basic())
-model2_task2 = lambda: (models.model2_cnn((9, 2)), task2_basic())
-model2_task3 = lambda: (models.model2_cnn((34, 2)), task3_basic())
-model2_task4 = lambda: (models.model2_cnn((26, 2)), task4_basic())
-model3_task1 = lambda: (models.model3_lstm((3, 2)), task1_basic())
-model3_task2 = lambda: (models.model3_lstm((9, 2)), task2_basic())
-model3_task3 = lambda: (models.model3_lstm((34, 2)), task3_basic())
-model3_task4 = lambda: (models.model3_lstm((26, 2)), task4_basic())
+TASK1_SHAPE = (3, 2)
+TASK2_SHAPE = (9, 2)
+TASK3_SHAPE = (34, 2)
+TASK4_SHAPE = (26, 2)
+TASK5_SHAPE = (25, 2)
+
+model1_task1 = lambda: (models.model1_mlp(TASK1_SHAPE), task1_basic())
+model1_task2 = lambda: (models.model1_mlp(TASK2_SHAPE), task2_basic())
+model1_task3 = lambda: (models.model1_mlp(TASK3_SHAPE), task3_basic())
+model1_task4 = lambda: (models.model1_mlp(TASK4_SHAPE), task4_basic())
+model1_task5 = lambda: (models.model1_mlp(TASK5_SHAPE), task5_basic())
+model2_task1 = lambda: (models.model2_cnn(TASK1_SHAPE), task1_basic())
+model2_task2 = lambda: (models.model2_cnn(TASK2_SHAPE), task2_basic())
+model2_task3 = lambda: (models.model2_cnn(TASK3_SHAPE), task3_basic())
+model2_task4 = lambda: (models.model2_cnn(TASK4_SHAPE), task4_basic())
+model2_task5 = lambda: (models.model2_cnn(TASK5_SHAPE), task5_basic())
+model3_task1 = lambda: (models.model3_lstm(TASK1_SHAPE), task1_basic())
+model3_task2 = lambda: (models.model3_lstm(TASK2_SHAPE), task2_basic())
+model3_task3 = lambda: (models.model3_lstm(TASK3_SHAPE), task3_basic())
+model3_task4 = lambda: (models.model3_lstm(TASK4_SHAPE), task4_basic())
+model3_task5 = lambda: (models.model3_lstm(TASK5_SHAPE), task5_basic())
 model4_task1 = lambda: model4(generator.Task1Generator())
 model4_task2 = lambda: model4(generator.Task2Generator())
 model4_task3 = lambda: model4(generator.Task3Generator())
 model4_task4 = lambda: model4(generator.Task4Generator())
+model4_task5 = lambda: model4(generator.Task5Generator())
 model5_task1 = lambda: model5(generator.Task1Generator())
 model5_task2 = lambda: model5(generator.Task2Generator())
 model5_task3 = lambda: model5(generator.Task3Generator())
 model5_task4 = lambda: model5(generator.Task4Generator())
+model5_task5 = lambda: model5(generator.Task5Generator())
 
 
 def run_test(args):
@@ -80,6 +92,8 @@ def run_test(args):
     )
 
     print(f'Complete model={args.model}, task={args.task}')
+
+    model.save(f'models/model{args.model}_task{args.task}')
 
 
 if __name__ == "__main__":
